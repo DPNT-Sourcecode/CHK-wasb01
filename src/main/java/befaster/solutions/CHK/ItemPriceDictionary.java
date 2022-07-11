3cb3ef246abc72a8d5120e0959c07f76a69ccb14
sbx_user1051 <sbx_user1051@169.254.12.77>
2022-07-11 14:48:36 +0000

    Mon Jul 11 14:48:36 UTC 2022

--- src/main/java/befaster/solutions/CHK/ItemPriceDictionary.java
+++ src/main/java/befaster/solutions/CHK/ItemPriceDictionary.java
@@ -7,6 +7,7 @@ public class ItemPriceDictionary {
 public static TreeMap<Character, Double> itemPriceMap = new TreeMap<Character, Double>();
 	
 	static{
+		itemPriceMap.put(null, 0.0);
 		itemPriceMap.put('A', 50.0);
 		itemPriceMap.put('B', 30.0);
 		itemPriceMap.put('C', 20.0);
@@ -14,3 +15,4 @@ public static TreeMap<Character, Double> itemPriceMap = new TreeMap<Character, D
 	}
 	
 }
+
